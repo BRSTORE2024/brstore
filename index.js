ea3e02aa510b8bf9bf468c876802f3d6d234d103
BR <166722865+BRSTORE2024@users.noreply.github.com>
2025-10-14 21:40:18 +0700

    update index.js

--- index.js
+++ index.js
@@ -83,7 +83,6 @@ const startSock = async () => {
 	})
 	
 	const pendingPwInput = new Map() // <sender, emailList[]>
-	const ownerJid = '6283832492541@s.whatsapp.net' // Ganti dengan punyamu
 
 	sock.ev.on('messages.upsert', async ({ messages, type }) => {
 		if (type !== 'notify') return
@@ -92,10 +91,6 @@ const startSock = async () => {
 		if (!msg.message) return
 
 		const sender = msg.key.remoteJid
-		const isFromOwner = sender === ownerJid || msg.key.participant === ownerJid
-		
-		if (!isFromOwner) return
-		
 		const text = (msg.message?.conversation ||
 			msg.message?.extendedTextMessage?.text ||
 			'').trim()
